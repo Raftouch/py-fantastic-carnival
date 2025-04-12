0adb0627b9ed054a7d59d69c9923da1d7f2d1a6a
raftouch <ntimorshina@gmail.com>
2025-04-12 22:35:17 +0200

    methods to reverse a list

---
+++ 3_methods_to_reverse_list.py
@@ -0,0 +1,13 @@
+numbers = [0,1,2,3,4,5,6,7]
+
+# 1
+numbers.reverse()
+# print(numbers)
+
+#2
+reversed_numbers = numbers[::-1]
+# print(reversed_numbers)
+
+# 3
+reversed_numbers = list(reversed(numbers))
+# print(reversed_numbers)
